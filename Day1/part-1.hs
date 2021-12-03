41ec71b5596f03d0dbb567c24f6d78323ba33c56
emmasTC <emmas.tech.corner@gmail.com>
2021-12-03 19:25:58 +0000

    Tidy up

--- Day1/part-1.hs
+++ Day1/part-1.hs
@@ -1,13 +1,15 @@
 import System.Environment
 import System.IO
 
-myFun :: [Int] -> Int -> [Int]
-myFun [] _ = []
-myFun (x:xs) a
- | x > a = x : myFun xs x
- | otherwise = myFun xs x
+largerThanPredecessor :: [Int] -> Int -> [Int]
+largerThanPredecessor [] _ = []
+largerThanPredecessor (x:xs) a
+ | x > a     = x : largerThanPredecessor xs x
+ | otherwise = largerThanPredecessor xs x
+
+
 
 main = do (fileName:_) <- getArgs
           contents <- readFile fileName
           let numList = map (read::String->Int) (words contents)
-          print $ length (myFun numList 0) -1
+          print $ length (largerThanPredecessor numList 0) -1 -- need to -1 for the first case as number always greater than 0
