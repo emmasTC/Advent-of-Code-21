62be794728879ce80f235a2daed313eb3f3bb4f5
emmasTC <emmas.tech.corner@gmail.com>
2021-12-03 20:57:15 +0000

    Part 2 completed by adding addFirstThree to add first 3 element in a list and threeList to create a list with the totals of each three consecutive elements

--- Day1/part-1.hs
+++ Day1/part-1.hs
@@ -7,9 +7,16 @@ largerThanPredecessor (x:xs) a
  | x > a     = x : largerThanPredecessor xs x
  | otherwise = largerThanPredecessor xs x
 
+addFirstThree :: [Int] -> Int -> Int -> Int
+addFirstThree [] _ _ = 0
+addFirstThree (x:xs) accumulator total =
+  if accumulator < 3 then addFirstThree xs (accumulator + 1) (total + x) else total
 
+threeList :: [Int] -> [Int]
+threeList [] = []
+threeList (x:xs) = addFirstThree (x:xs) 0 0: threeList xs
 
 main = do (fileName:_) <- getArgs
           contents <- readFile fileName
           let numList = map (read::String->Int) (words contents)
-          print $ length (largerThanPredecessor numList 0) -1 -- need to -1 for the first case as number always greater than 0
+          print $ length (largerThanPredecessor (threeList numList) 0)
