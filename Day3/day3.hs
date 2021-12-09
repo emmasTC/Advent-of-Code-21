01a04fd74ba3b96d7edd17450d8f882a6fc7f33f
emmasTC <emmas.tech.corner@gmail.com>
2021-12-09 17:10:29 +0000

    Day 3 part 1 completed

--- Day3/day3.hs
+++ Day3/day3.hs
@@ -1,4 +1,5 @@
 import System.Environment
+import Data.Char(digitToInt)
 
 totalOnes:: [String] -> Int -> [Int]
 totalOnes [] _ = []
@@ -22,24 +23,16 @@ epsilon (x:xs) listLen =
 
 binToDec :: [(Char, Int)] -> Int
 binToDec [] = 0
-binToDec (x:xs) = 2 ^ ((snd x)-1) + binToDec xs
+binToDec (x:xs) = (digitToInt (fst x)) * (2 ^ ((snd x)-1)) + binToDec xs
 
 main = do (fileName:_) <- getArgs
           contents <- readFile fileName
           let strbnums = (lines contents)
-          print $ strbnums
           let listLen = length strbnums
-          print $ listLen
-          print $ length (filter (\n -> (drop (1 - 1) (take 1 n)) == "1") strbnums)
           let onesList = reverse $ totalOnes strbnums 12
-          print $ gamma onesList listLen
-          print $ epsilon onesList listLen
           let gammaBin =  gamma onesList listLen
           let epsilonBin = epsilon onesList listLen
-          print $ zip gammaBin [12,11..]
           let gammaZip = zip gammaBin [12,11..]
-          let binOneList= (filter (\n -> n == ('1', 1)) gammaZip) ++ (filter (\n -> n == ('1', 2)) gammaZip) ++ (filter (\n -> n == ('1', 3)) gammaZip) ++ (filter (\n -> n == ('1', 4)) gammaZip) ++ (filter (\n -> n == ('1', 5)) gammaZip) ++ (filter (\n -> n == ('1', 6)) gammaZip) ++ (filter (\n -> n == ('1', 7)) gammaZip) ++ (filter (\n -> n == ('1', 8)) gammaZip) ++ (filter (\n -> n == ('1', 9)) gammaZip) ++ (filter (\n -> n == ('1', 10)) gammaZip) ++ (filter (\n -> n == ('1', 11)) gammaZip) ++ (filter (\n -> n == ('1', 12)) gammaZip)
-          print $ binToDec binOneList
-          --print $ getInt (head binOneList)
-          --print $ binToDec gammaZip 5
-          --print $ filter (\n -> n == ('1', [1..])) gammaZip
+          let epsilonZip = zip epsilonBin [12,11..]
+          print $ (binToDec gammaZip) * (binToDec epsilonZip)
+        
