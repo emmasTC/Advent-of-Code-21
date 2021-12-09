c4f2301d73b495c3710c2a500d76fdd31e0818d1
emmasTC <emmas.tech.corner@gmail.com>
2021-12-09 13:28:13 +0000

    Binry to decimal convertor working

--- Day3/day3.hs
+++ Day3/day3.hs
@@ -20,6 +20,9 @@ epsilon (x:xs) listLen =
      then "0" ++ epsilon xs listLen
   else "1" ++ epsilon xs listLen
 
+binToDec :: [(Char, Int)] -> Int
+binToDec [] = 0
+binToDec (x:xs) = 2 ^ ((snd x)-1) + binToDec xs
 
 main = do (fileName:_) <- getArgs
           contents <- readFile fileName
@@ -28,15 +31,15 @@ main = do (fileName:_) <- getArgs
           let listLen = length strbnums
           print $ listLen
           print $ length (filter (\n -> (drop (1 - 1) (take 1 n)) == "1") strbnums)
-          let onesList = reverse $ totalOnes strbnums 5
+          let onesList = reverse $ totalOnes strbnums 12
           print $ gamma onesList listLen
           print $ epsilon onesList listLen
           let gammaBin =  gamma onesList listLen
           let epsilonBin = epsilon onesList listLen
-          print $ zip gammaBin [5,4..]
-          let gammaZip = zip gammaBin [5,4..]
-          let binOneList= (filter (\n -> n == ('1', 1)) gammaZip) ++ (filter (\n -> n == ('1', 2)) gammaZip) ++ (filter (\n -> n == ('1', 3)) gammaZip) ++ (filter (\n -> n == ('1', 4)) gammaZip) ++ (filter (\n -> n == ('1', 5)) gammaZip)
-          print $ head binOneList
-          print $ getInt (head binOneList)
+          print $ zip gammaBin [12,11..]
+          let gammaZip = zip gammaBin [12,11..]
+          let binOneList= (filter (\n -> n == ('1', 1)) gammaZip) ++ (filter (\n -> n == ('1', 2)) gammaZip) ++ (filter (\n -> n == ('1', 3)) gammaZip) ++ (filter (\n -> n == ('1', 4)) gammaZip) ++ (filter (\n -> n == ('1', 5)) gammaZip) ++ (filter (\n -> n == ('1', 6)) gammaZip) ++ (filter (\n -> n == ('1', 7)) gammaZip) ++ (filter (\n -> n == ('1', 8)) gammaZip) ++ (filter (\n -> n == ('1', 9)) gammaZip) ++ (filter (\n -> n == ('1', 10)) gammaZip) ++ (filter (\n -> n == ('1', 11)) gammaZip) ++ (filter (\n -> n == ('1', 12)) gammaZip)
+          print $ binToDec binOneList
+          --print $ getInt (head binOneList)
           --print $ binToDec gammaZip 5
           --print $ filter (\n -> n == ('1', [1..])) gammaZip
